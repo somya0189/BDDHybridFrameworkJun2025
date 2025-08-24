b862886f21465aa69082cad51319d3341e1986bc
somya0189 <somya.gupta0104@gmail.com>
2025-08-24 09:23:53 +0530

    Updated Base.Java class

--- src/test/java/com/qa/base/Base.java
+++ src/test/java/com/qa/base/Base.java
@@ -1,35 +1,23 @@
 package com.qa.base;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import com.qa.util.ReadProperties;
 
 /**
  * @author sony
  * This class have methods for open and close the browser
  */
-/**
- * @author sony
- *
- */
-/**
- * @author sony
- *
- */
+
 public class Base {
 	
 	public static WebDriver driver;
-
-
 	/**
 	 * @return the driver object after launching the application URL
 	 */
 	public WebDriver initializeWebDriver() {
 
-		
 		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
 
 		driver = new ChromeDriver();
@@ -41,12 +29,8 @@ public class Base {
 		driver.get(ReadProperties.getappURL());
 
 		return driver;
-
- 
-
 	}
 	
-
 	/**
 	 * This method is for closing the browser
 	 */
