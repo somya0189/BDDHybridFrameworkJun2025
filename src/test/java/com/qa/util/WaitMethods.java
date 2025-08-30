5f32ea69e80e8bb789ca2e7c6587eae14af5fedd
somya0189 <somya.gupta0104@gmail.com>
2025-08-30 08:39:41 +0530

    Added WaitMethods class

---
+++ src/test/java/com/qa/util/WaitMethods.java
@@ -0,0 +1,93 @@
+package com.qa.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import cucumber.api.Scenario;
+
+public class WaitMethods {
+	
+	WebDriver driver;
+	
+	public void staticWait(long milisec){
+
+		try {
+
+			Thread.sleep(milisec);
+
+		} 
+		catch (InterruptedException e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+	}
+	public static WebElement waitFor(WebDriver driver, WebElement elem, String waitType, Scenario scenario) {
+
+		 
+
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+
+				.withTimeout(ReadProperties.getFluentWaitTime(), TimeUnit.SECONDS)
+
+				.pollingEvery(60, TimeUnit.MILLISECONDS).ignoring(NoSuchElementException.class);
+
+		try {
+
+			switch (waitType) {
+
+			case "elementTobeClickable":
+
+				wait.until(ExpectedConditions.elementToBeClickable(elem));
+
+				break;
+
+			case "visibilityOf":
+
+				wait.until(ExpectedConditions.visibilityOf(elem));
+
+				break;
+
+			case "elementToBeSelected":
+
+				wait.until(ExpectedConditions.elementToBeSelected(elem));
+
+				break;
+
+			case "stalenessof":
+
+				wait.until(ExpectedConditions.stalenessOf(elem));
+
+				break;
+
+			default:
+
+				wait.until(ExpectedConditions.visibilityOf(elem));
+
+			}
+
+ 
+
+		} catch (Exception E) {
+
+ 
+
+			scenario.write("There is some Exception in Fluent Wait checking--  " + E.getMessage());
+
+		}
+
+ 
+
+		return elem;
+
+	}
+}
