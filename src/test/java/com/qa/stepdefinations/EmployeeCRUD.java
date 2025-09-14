df86340f58975250001ced46370d3600aa7ed987
somya0189 <somya.gupta0104@gmail.com>
2025-09-14 11:08:44 +0530

    Modified EmployeeCrud.java and WaitMethods.java file

--- src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
+++ src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
@@ -1,24 +1,51 @@
 package com.qa.stepdefinations;
 
+import com.qa.base.Base;
+import com.qa.pages.LoginPage;
+import com.qa.util.CaptureScreenshot;
+import com.qa.util.WaitMethods;
+
 import cucumber.api.DataTable;
-import cucumber.api.PendingException;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class EmployeeCRUD {
+public class EmployeeCRUD extends Base {
+
+	Scenario scenario;
+	LoginPage objLoginPage;
+
+	@Before
+
+	public void logintoApplication(Scenario scenario) {
+
+		this.scenario = scenario;
+
+	}
+
 	@Given("^Navigate to PIM after log in with Admin user$")
 	public void navigate_to_PIM_after_log_in_with_Admin_user() throws Throwable {
-	  System.out.println("Selenium code is added here");
+		scenario.write("Starting the Orange HRM application in browser");
+
+		driver = initializeWebDriver();
+
+		WaitMethods.staticWait(5000);
+
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
 	@When("^I Add employee with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
-	public void i_Add_employee_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2, String arg3) throws Throwable {
+	public void i_Add_employee_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2, String arg3)
+			throws Throwable {
 		System.out.println("Selenium code is added here");
 	}
 
 	@Then("^I  verify employeeAdded in list with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
-	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2, String arg3) throws Throwable {
+	public void i_verify_employeeAdded_in_list_with_first_name_as_and_mname_as_and_lName_as(String arg1, String arg2,
+			String arg3) throws Throwable {
 		System.out.println("Selenium code is added here");
 	}
 
@@ -28,12 +55,25 @@ public class EmployeeCRUD {
 	}
 
 	@Then("^I search the employee and ensure that it is searched using below values$")
-	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable arg1) throws Throwable {
+	public void i_search_the_employee_and_ensure_that_it_is_searched_using_below_values(DataTable arg1)
+			throws Throwable {
 		System.out.println("Selenium code is added here");
 	}
 
 	@Then("^I select and Delete the Updated Employee and verify employee is not  in search result$")
-	public void i_select_and_Delete_the_Updated_Employee_and_verify_employee_is_not_in_search_result() throws Throwable {
+	public void i_select_and_Delete_the_Updated_Employee_and_verify_employee_is_not_in_search_result()
+			throws Throwable {
 		System.out.println("Selenium code is added here");
 	}
+
+	@After
+
+	public void closeApplication(Scenario scenario) {
+
+		scenario.write("Closing the application");
+
+		closeBrowser();
+
+	}
+
 }
--- src/test/java/com/qa/util/WaitMethods.java
+++ src/test/java/com/qa/util/WaitMethods.java
@@ -11,105 +11,63 @@ import org.openqa.selenium.support.ui.Wait;
 
 import cucumber.api.Scenario;
 
+/**
+ * @author user This class has common method to configure the implicit &
+ *         explicit wait times
+ */
 public class WaitMethods {
 	
 	WebDriver driver;
-	
+
+	public static String ELEMENT_TO_BE_CLICKABLE = "elementTobeClickable";
+	public static String ELEMENT_TO_BE_VISIBLE = "visibilityOf";
+	public static String STALENESS_OF_ELEMENT = "stalenessof";
+
 	/**
-	 * @author  This class has common methods to configure the implicit and
-	 *         explicit wait times.
-	 *
+	 * @param milisec
+	 * This method will help to 
 	 */
-
-
-
-		public static String ELEMENT_TO_BE_CLICKABLE = "elementTobeClickable";
-		public static String ELEMENT_TO_BE_VISIBLE = "visibilityOf";
-		public static String STALENESS_OF_ELEMENT = "stalenessof";
-
-		/**
-		 * @param milisec
-		 *            This method will help to wait for specified time
-		 */
-	
-	public void staticWait(long milisec){
-
+	public static void staticWait(long milisec) {
 		try {
-
 			Thread.sleep(milisec);
-
-		} 
-		catch (InterruptedException e) {
-
-			
-
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-
 		}
-
 	}
-	/**
-	 * @author This method will wait for the condition of specific element
-	 *         for a configured time in config.properties file.
-	 *
-	 */
+	
+	
 	public static WebElement waitFor(WebDriver driver, WebElement elem, String waitType, Scenario scenario) {
 
-		 
-
+		@SuppressWarnings("deprecation")
 		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-
 				.withTimeout(ReadProperties.getFluentWaitTime(), TimeUnit.SECONDS)
-
 				.pollingEvery(60, TimeUnit.MILLISECONDS).ignoring(NoSuchElementException.class);
-
 		try {
-
 			switch (waitType) {
-
 			case "elementTobeClickable":
-
 				wait.until(ExpectedConditions.elementToBeClickable(elem));
-
 				break;
-
 			case "visibilityOf":
-
 				wait.until(ExpectedConditions.visibilityOf(elem));
-
 				break;
-
 			case "elementToBeSelected":
-
 				wait.until(ExpectedConditions.elementToBeSelected(elem));
-
 				break;
-
 			case "stalenessof":
-
 				wait.until(ExpectedConditions.stalenessOf(elem));
-
 				break;
-
 			default:
-
 				wait.until(ExpectedConditions.visibilityOf(elem));
-
 			}
-
- 
 
 		} catch (Exception E) {
 
- 
-
 			scenario.write("There is some Exception in Fluent Wait checking--  " + E.getMessage());
-
 		}
 
- 
-
 		return elem;
-
 	}
+	
+
 }
