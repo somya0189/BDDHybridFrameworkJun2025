3ddd0e643b057ab65ad6fe4bedb5a99ac18359fd
somya0189 <somya.gupta0104@gmail.com>
2025-10-17 23:41:25 +0530

    Modified AddEmployeePage.java class

--- src/test/java/com/qa/pages/AddEmployeePage.java
+++ src/test/java/com/qa/pages/AddEmployeePage.java
@@ -1,7 +1,9 @@
 package com.qa.pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -34,6 +36,60 @@ public class AddEmployeePage {
 	@FindBy(xpath = "//button[text()=' Save ']")
     WebElement saveButton;
 	
+	// page repo for search emp
+
+	@FindBy(xpath = "//a[text()='Employee List']")
+
+	WebElement empListPageLink;
+
+	@FindBy(xpath = "//label[text()='Employee Name']/following::input[1]")
+
+	WebElement searchByEmpNamefield;
+
+	@FindBy(xpath = "//button[text()=' Search ']")
+
+	WebElement empSearchButton;
+
+	@FindBy(xpath = "//div[@class='oxd-table-row oxd-table-row--with-border oxd-table-row--clickable']/child::div[3]/child::div[1]")
+
+	WebElement searchedEmpFandMname;
+	
+	// page repo for edit emp
+
+	@FindBy(xpath = "//div[@class='oxd-table-cell-actions']/child::button/i[@class='oxd-icon bi-pencil-fill']")
+
+	WebElement editEmpButton;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='firstName']")
+
+	WebElement empfirstName;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='middleName']")
+
+	WebElement empmiddleName;
+
+	@FindBy(xpath = "//label[text()='Employee Full Name']/following::input[@name='lastName']")
+
+	WebElement emplastName;
+
+	@FindBy(xpath="//button[text()=' Save ']")
+
+	WebElement editandSaveButton;
+	
+	// page Repository for delete emp
+
+	@FindBy(xpath = "//div[@class='oxd-table-cell-actions']/child::button/i[@class='oxd-icon bi-trash']")
+
+	WebElement deleteSearchedEmp;
+
+	@FindBy(xpath = "//button[text()=' Yes, Delete ']")
+
+	WebElement deleteConfirmButton;
+
+	@FindBy(xpath = "//span[text()='No Records Found']")
+
+	WebElement noRecordFoundTextAfterDelete;
+	
 	
 	//Page class constructor
 	public AddEmployeePage(WebDriver driver, Scenario scenario) {
@@ -44,13 +100,13 @@ public class AddEmployeePage {
 }
 	
 	// Page Class Operations
-	
-	public void navigateToPimPage() {
 
+	public void navigateToPimPage() {
+		WaitMethods.staticWait(5000);
 		ElementActions.clickElement(driver, pimPageLink, scenario);
 
 	}
-	
+
 	public void addnewEmployee(String fName, String mName, String lName) {
 
 		ElementActions.clickElement(driver, addEmpButton, scenario);
@@ -66,5 +122,61 @@ public class AddEmployeePage {
 		ElementActions.clickElement(driver, saveButton, scenario);
 
 	}
-    
+
+	public void navigateToEmpListPage() {
+
+		ElementActions.clickElement(driver, empListPageLink, scenario);
+
+	}
+
+	public String searchEmp(String fName, String mName) {
+
+		WaitMethods.staticWait(5000);
+
+		ElementActions.sendKeys(driver, searchByEmpNamefield, scenario, fName + " " + mName);
+
+		WaitMethods.staticWait(2000);
+
+		Actions objactions = new Actions(driver);
+
+		objactions.sendKeys(Keys.ARROW_DOWN).build().perform();
+
+		WaitMethods.staticWait(2000);
+
+		ElementActions.clickElement(driver, empSearchButton, scenario);
+
+		return ElementActions.getText(driver, searchedEmpFandMname, scenario);
+
+	}
+
+	public void editEmp(String fnameupendtext, String mNameupendtext, String lNameupendtext) {
+
+		ElementActions.clickElement(driver, editEmpButton, scenario);
+
+		WaitMethods.staticWait(5000);
+
+		ElementActions.sendKeys(driver, empfirstName, scenario, fnameupendtext);
+
+		ElementActions.sendKeys(driver, empmiddleName, scenario, mNameupendtext);
+
+		ElementActions.sendKeys(driver, emplastName, scenario, lNameupendtext);
+
+		WaitMethods.staticWait(5000);
+
+		ElementActions.clickElement(driver, editandSaveButton, scenario);
+
+	}
+
+	public String deleteUdpateEmp() {
+
+		ElementActions.clickElement(driver, deleteSearchedEmp, scenario);
+
+		ElementActions.clickElement(driver, deleteConfirmButton, scenario);
+
+		WaitMethods.staticWait(5000);
+
+		return ElementActions.getText(driver, noRecordFoundTextAfterDelete, scenario);
+
+	}
+
 }
